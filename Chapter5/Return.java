f3f271fb114a7584ed18fbde625f507f9ca8f080
Chang Liu <MissingPositron@users.noreply.github.com>
2017-12-01 09:14:09 +0800

    Chapter 5 done
    Chapter 5 done

---
+++ Chapter5/Return.java
@@ -0,0 +1,12 @@
+// Demonstrate return
+class Return {
+    public static void main(String args[]) {
+        boolean t = true;
+
+        System.out.println("Before the return.");
+
+        if (t) return; // return the caller
+
+        System.out.println("This won't execute.");
+    }
+}
