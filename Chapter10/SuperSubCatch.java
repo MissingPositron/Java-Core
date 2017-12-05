a0f9acbfc0a37f9db47a393c1c3d142ba7f0fee6
Chang Liu <MissingPositron@users.noreply.github.com>
2017-12-05 09:47:36 +0800

    Chapter 10 done
    Chapter 10 done

---
+++ Chapter10/SuperSubCatch.java
@@ -0,0 +1,21 @@
+/* This program contains an error.abstract
+    A subclass must come before its superclass in 
+    a series of catch statements. If not,
+    unreachable code will be created and a 
+    compile-time error will result.abstract*/
+class SuperSubCatch {
+    public static void main (String args[]) {
+        try {
+            int a = 0;
+            int b = 42 / a;
+        } catch (Exception e) {
+            System.out.println("Generic Exception catch.");
+        }
+        /* This catch is never reached because 
+            ArithmeticException is a subclass of Exception.*/
+        catch(ArithmeticException e) {
+            // Error - unreachable
+            System.out.println("This is never reached.");
+        }
+    }
+}
